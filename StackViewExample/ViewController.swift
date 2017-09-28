8b020293980512752a41005d35cd17dd015b67c6
Madeline Webster <Madeline@Madelines-MacBook-Pro.local>
2017-09-28 08:08:25 -0700

    Initial Commit

---
+++ StackViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewExample
+//
+//  Created by Madeline Webster on 9/28/17.
+//  Copyright © 2017 Madeline Webster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
